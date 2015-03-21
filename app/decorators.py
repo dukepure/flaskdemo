0812eed59a1f1c213e0641f253fba74ee526a1f7
dukepure <wangqiang@yandex.ru>
2015-03-21 16:08:12 +0800

    flask study

---
+++ app/decorators.py
@@ -0,0 +1,21 @@
+__author__ = 'duke'
+
+from functools import wraps
+from flask import abort
+from flask.ext.login import current_user
+from .models import Permission
+
+
+def permission_required(permission):
+    def decprator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.can(permission):
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decprator
+
+
+def admin_required(f):
+    return permission_required(Permission.ADMINISTER)(f)
